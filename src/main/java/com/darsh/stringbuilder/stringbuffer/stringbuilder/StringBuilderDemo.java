aaf0f677b6a7b1dff1463237e37d298daf77a19b
sudarshan-doiphode <doiphode.sudarshan3@gmail.com>
2024-03-08 09:19:39 +0530

    Initial commit

---
+++ src/main/java/com/darsh/stringbuilder/stringbuffer/stringbuilder/StringBuilderDemo.java
@@ -0,0 +1,7 @@
+package com.darsh.stringbuilder.stringbuffer.stringbuilder;
+
+public class StringBuilderDemo {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+    }
+}
---
+++ src/test/java/com/darsh/stringbuilder/stringbuffer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darsh.stringbuilder.stringbuffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
